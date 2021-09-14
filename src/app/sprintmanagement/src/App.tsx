a83a15c0dbb01adc8798cbee9fb96ab74c534b78
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2021-09-14 13:25:34 +0430

    base project

---
+++ src/app/sprintmanagement/src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <Box>
+      <Grid>
+        <GridItem>Hello There</GridItem>
+      </Grid>
+    </Box>
+  );
+}
+
+export default App;
